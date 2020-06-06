41666fb191fc5ebf166d95d459f02da4b2858bc8
MoisesLopezVan <figueroalpezverificacion@gmail.com>
2020-06-06 01:36:24 -0500

    Subiendo comentarios

--- EjemplosGenerica/src/ejemplosgenerica/Principal.java
+++ EjemplosGenerica/src/ejemplosgenerica/Principal.java
@@ -24,5 +24,28 @@ public class Principal {
         cajaVerdura.showType();
         cajaPersona.showType();
         
+        /*Lista<String> lista = new Lista<>();
+        
+        System.out.println("sList - print linkedlist: " + lista);
+        System.out.println("sList.size() - print linkedlist size: " + lista.size());
+        
+        lista.add("Lista1");
+        lista.add("Lista2");
+        lista.add("Lista3");
+        lista.add("Lista4");
+        lista.add("Lista5");
+        lista.add("Lista6");
+        lista.add("Lista7");
+        lista.add("Lista8");
+        lista.add("Lista9");
+        lista.add("Lista10");
+       
+        //System.out.println("Datos: "+lista.get(1));
+        System.out.println("sList.size() - print linkedlist size: " + lista.size());
+        System.out.println("sList.get(3) - get 3rd element: " + lista.get(3));
+        System.out.println("sList.remove(2) - remove 2nd element: " + lista.remove(2));
+        System.out.println("sList.get(3) - get 3rd element: " + lista.get(3));
+        System.out.println("sList.size() - print linkedlist size: " + lista.size());
+        System.out.println("sList - print linkedlist: " + lista);*/
     }
 }
